91f52e67d459c0103f36a04ae1de69d8dd6f7ca3
Araby <arabyzhou@gmail.com>
2021-02-23 16:20:25 +0800

    First dot Net Core Application

---
+++ _MyAspNetCoreProject/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (guestResponse.WillAttend == true)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponse()
+        {
+            return View(Repository.Responses.Where(e => e.WillAttend == true));
+        }
+    }
+}
---
+++ _MyAspNetCoreProject/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter Name")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Email")]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter Address")]
+        public String Address { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public Boolean? WillAttend { get; set; }
+    }
+}

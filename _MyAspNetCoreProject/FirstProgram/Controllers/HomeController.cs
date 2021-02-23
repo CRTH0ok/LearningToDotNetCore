ae853e9899f9e26a30788099afc0835821faf76b
Araby <arabyzhou@gmail.com>
2021-02-23 09:46:44 +0800

    Getting Started

---
+++ _MyAspNetCoreProject/FirstProgram/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using FirstProgram.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProgram.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public String Index()
+        //{
+        //    return "Hello World!";
+        //}
+        public ViewResult Index()
+        {
+            String viewModel = DateTime.Now.Hour > 12 ? "Good Afternoon!" : "Good Morning";
+            return View("MyView",viewModel);
+        }
+    }
+}
---
+++ _MyAspNetCoreProject/FirstProgram/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstProgram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

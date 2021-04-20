d36c855e97d194d917c455b4295d0a01041207f5
Araby <arabyzhou@gmail.com>
2021-04-20 14:03:05 +0800

    synchronization

--- _MyAspNetCoreProject/SportsStore/Migrations/20210308073344_Initial.cs
+++ _MyAspNetCoreProject/SportsStore/Migrations/20210308073344_Initial.cs
@@ -6,21 +6,21 @@ namespace SportsStore.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Products",
-                columns: table => new
-                {
-                    ProductID = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Price = table.Column<decimal>(type: "Decimal(8,2)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Products", x => x.ProductID);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Products",
+            //    columns: table => new
+            //    {
+            //        ProductID = table.Column<long>(type: "bigint", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        Price = table.Column<decimal>(type: "Decimal(8,2)", nullable: false),
+            //        Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        Category = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Products", x => x.ProductID);
+            //    });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
